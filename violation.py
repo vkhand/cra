83c61f42c47ea5a34656c5beb573f5d489ff2ee3
kartik <kartik1702@gmail.com>
2019-04-05 05:23:41 +0530

    Made api, pass vehicle no.

--- violation.py
+++ violation.py
@@ -1,18 +1,29 @@
 import bs4
 import requests
 import json
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/api', methods=['GET'])
+def crawler():
 
 
-def crawler(vehical_no):
-    try:
-        s=requests.Session()
-        payload = {'SearchBy':'REGNO',
-                   'SearchValue': vehical_no,
-                    'ServiceCode': 'BPS'}
-        page = requests.post("https://www.karnatakaone.gov.in/PoliceCollectionOfFine/FineDetails?SearchBy=REGNO&SearchValue=ka03mq9396&ServiceCode=BPS", data=payload)
-        soup = bs4.BeautifulSoup(page.text,"html.parser")
-        d=json.loads(str(soup))
-        return d;
-    except IndexError:
-        return None
+    vehical_no = request.args.get('vehicleNo')
+    s=requests.Session()
+    payload = {'SearchBy':'REGNO',
+                'SearchValue': vehical_no,
+                'ServiceCode': 'BPS'}
+    page = requests.post("https://www.karnatakaone.gov.in/PoliceCollectionOfFine/FineDetails?SearchBy=REGNO&SearchValue="+str(vehical_no)+"&ServiceCode=BPS", data=payload)
+    soup = bs4.BeautifulSoup(page.text,"html.parser")
+    d=json.loads(str(soup))
+    return jsonify(d)
+    
+if __name__=="__main__":
+    app.run()
+	
+	
+
 
